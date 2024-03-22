0448bbbd294efccfe702410dc931c9a479541f38
Raphael Bouças <rafaladob@gmail.com>
2024-03-21 22:04:14 -0300

    componentes

---
+++ src/components/Cabecalho.vue
@@ -0,0 +1,26 @@
+<!-- <script setup>
+    const props = defineProps(['tarefasPendentes']);
+</script>
+
+<template>
+    <header class="p-5 mb-4 mt-4 bg-light rounded-3">
+        <h1>Minhas tarefas</h1>
+        <p>
+            Você possui {{ props.tarefasPendentes }} tarefas pendentes.  (antes, em vez de props.tarefasPendentes, era 'getTarefasPendentes().length' )
+        </p>
+    </header>
+</template> 
+-->
+
+<script setup>
+    const props = defineProps(['tarefasPendentes']);
+</script>
+
+<template>
+    <header class="p-5 mb-4 mt-4 bg-light rounded-3">
+        <h1>Minhas tarefas</h1>
+        <p>
+            Você possui {{ props.tarefasPendentes }} tarefas pendentes. <!-- antes, em vez de props.tarefasPendentes, era 'getTarefasPendentes().length' -->
+        </p>
+    </header>
+</template>
